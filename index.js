f7ee8eb34322281f55c17818fc2d15fda8584130
kiranmohan1994 <kiranmohankm1994@gmail.com>
2024-11-23 14:10:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const mainTitle = document.getElementById('main-title');
+console.log(mainTitle);
+
+const containers = document.getElementsByClassName('containers');
+console.log(containers);
+
+const sections = document.getElementsByClassName('section');
+console.log(sections);
+
+const paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs);
+
+const firstButton = document.querySelector('.btn');
+console.log(firstButton);
+
+const firstSection = document.querySelector('.section');
+console.log(firstSection);
+
+const allButtons = document.querySelectorAll('.btn');
+console.log(allButtons);
+
+const allParagraphsInContainer = document.querySelectorAll('.container p');
+console.log(allParagraphsInContainer);
